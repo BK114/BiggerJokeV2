09e993a48e134afc98019783ae6dcb3df7a686af
Brock <brock.w.rice@gmail.com>
2018-02-26 08:59:45 +0000

    update fragment
    Modify fragments to contain logic in order to start AsyncTask and retrieve joke. added button listener to fragments since main_fragment.xml contains the button.

--- app/src/paid/java/com/udacity/gradle/builditbigger/paid/MainActivityFragmentPaid.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/paid/MainActivityFragmentPaid.java
@@ -24,6 +24,18 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         final View root = inflater.inflate(R.layout.fragment_main, container, false);
 
+        Button jokeButton = root.findViewById(R.id.btn_joke);
+        jokeButton.setOnClickListener(New View.OnclickListener(){
+          @Override
+            public void onClick(){
+                
+                getJokes();
+                
+            )
+        }); 
         return root;
+    }
+              public void getJokes() {
+                  new EndpointsAsyncTask().execute(getActivity());
     }
 }
